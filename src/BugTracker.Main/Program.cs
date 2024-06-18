9d3156da6f2d0f424b1846b5e2274df2cace4239
Muhammad Babayi <chief.mb1976@gmail.com>
2024-06-18 23:59:52 +0330

    feat: Add HTTPS in pipeline

--- src/BugTracker.Main/Program.cs
+++ src/BugTracker.Main/Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.UseHttpsRedirection();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
