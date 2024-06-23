c0bbd1c6ae9c023af4270e0e52179e35d319c628
Muhammad Babayi <chief.mb1976@gmail.com>
2024-06-23 17:23:42 +0330

    feat: Add and use GihanSoft.Framework.Web

--- src/BugTracker.Main/Program.cs
+++ src/BugTracker.Main/Program.cs
@@ -1,18 +1,21 @@
+using GihanSoft.Framework.Web.Bootstrap.ConditionalPipelineUse;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddExceptionHandler(opt => opt.ExceptionHandlingPath = "/error");
+
 var app = builder.Build();
 
-app.UseHttpsRedirection();
+var isDevelopment = app.Environment.IsDevelopment();
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseHsts();
-}
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
+_ = app
+    .If(isDevelopment, a => a.UseDeveloperExceptionPage())
+    .If(!isDevelopment, a => a.UseExceptionHandler())
+    .If(!isDevelopment, a => a.UseHsts())
+    .UseHttpsRedirection()
+    ;
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/error", () => "There is an error!");
 
 app.Run();
