94bfba08a18c778e9fb26fa2a7ac369a7e491d76
Muhammad Babayi <chief.mb1976@gmail.com>
2024-07-12 11:54:53 +0330

    feat: Add configurable `ForwardedHeaders` middleware

--- src/BugTracker.Main/Program.cs
+++ src/BugTracker.Main/Program.cs
@@ -1,9 +1,12 @@
 using BugTracker.Main.Components;
 using BugTracker.Main.Features.Identity.Startup;
+
 using GihanSoft.Framework.Web.Bootstrap.ConditionalPipelineUse;
 using GihanSoft.Framework.Web.Bootstrap.Initialization;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<ForwardedHeadersOptions>(builder.Configuration.GetSection(nameof(ForwardedHeadersOptions)));
 
 builder.Services.AddRazorComponents();
 builder.Services.AddIdentityServices(builder.Configuration);
@@ -14,6 +17,7 @@ var isDevelopment = app.Environment.IsDevelopment();
 _ = app
     .If(isDevelopment, a => a.UseDeveloperExceptionPage())
     .If(!isDevelopment, a => a.UseExceptionHandler("/500"))
+    .UseForwardedHeaders()
     .If(!isDevelopment, a => a.UseHsts())
     .UseHttpsRedirection()
     .UseStatusCodePagesWithReExecute("/{0}")
