67197e797f6c967120a6284ed7f20c3bcbaf4d61
Muhammad Babayi <chief.mb1976@gmail.com>
2024-08-22 12:01:59 +0330

    feat: Move project querying to Mediator

---
+++ src/BugTracker.Main/Features/Backlog/Mediator/QueryProjects.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+
+using BugTracker.Main.Features.Backlog.Data;
+
+using MediatR;
+
+using Microsoft.EntityFrameworkCore;
+
+using Riok.Mapperly.Abstractions;
+
+namespace BugTracker.Main.Features.Backlog.Mediator;
+
+[Mapper]
+internal static partial class QueryProjects
+{
+    public record Request : IRequest<Either<Error, Response>>;
+    public record Response(IReadOnlyCollection<Response.Project> Projects)
+    {
+        public record Project(string OwnerKey, string Key);
+    }
+
+    public class Handler(
+        IHttpContextAccessor httpContextAccessor,
+        BacklogDbContext _db)
+        : IRequestHandler<Request, Either<Error, Response>>
+    {
+        private readonly HttpContext _httpContext = httpContextAccessor.HttpContext
+            ?? throw new InvalidOperationException();
+        private readonly BacklogDbContext _db = _db;
+
+        public async Task<Either<Error, Response>> Handle(Request request, CancellationToken ct)
+        {
+            if (_httpContext.User.Identity?.Name is not string username)
+            {
+                return Error.New(new UnreachableException());
+            }
+
+            var baseQuery = _db.Projects.Where(x => x.OwnerKey == username);
+            var projects = await baseQuery.Map().ToListAsync(ct);
+            return new Response(projects.AsReadOnly());
+        }
+    }
+
+    private static partial IQueryable<Response.Project> Map(this IQueryable<Project> project);
+}
