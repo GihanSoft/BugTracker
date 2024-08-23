5db6f8eda6c69237cdce6f7ff41fe82f73c0bb21
Muhammad Babayi <chief.mb1976@gmail.com>
2024-08-23 11:20:07 +0330

    feat: move signin to mediator

---
+++ src/BugTracker.Main/Features/Identity/Mediator/SignInOp.cs
@@ -0,0 +1,27 @@
+﻿
+using MediatR;
+
+using Microsoft.AspNetCore.Identity;
+
+namespace BugTracker.Main.Features.Identity.Mediator;
+
+internal static class SignInOp
+{
+    public record Request(string Username, string Password, bool Remember) : IRequest<Either<Error, SignInResult>>;
+    public class Handler(SignInManager<AppUser> _signInManager)
+        : IRequestHandler<Request, Either<Error, SignInResult>>
+    {
+        private readonly SignInManager<AppUser> _signInManager = _signInManager;
+
+        public async Task<Either<Error, SignInResult>> Handle(Request request, CancellationToken cancellationToken)
+        {
+            var result = await _signInManager.PasswordSignInAsync(
+                request.Username,
+                request.Password,
+                request.Remember,
+                true);
+
+            return result;
+        }
+    }
+}
