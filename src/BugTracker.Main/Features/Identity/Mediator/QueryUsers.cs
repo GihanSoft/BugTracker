bdb6cf2d7b80bafc750a7810ca76472e37d95802
Muhammad Babayi <chief.mb1976@gmail.com>
2024-08-23 11:38:49 +0330

    move user querying to mediator

---
+++ src/BugTracker.Main/Features/Identity/Mediator/QueryUsers.cs
@@ -0,0 +1,43 @@
+﻿
+using MediatR;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+using Riok.Mapperly.Abstractions;
+
+namespace BugTracker.Main.Features.Identity.Mediator;
+
+[Mapper]
+internal static partial class QueryUsers
+{
+    public record Request() : IRequest<Either<Error, Response>>;
+    public record Response(IReadOnlyCollection<Response.User> Users)
+    {
+        public record User(
+            string Id,
+            string Username,
+            string Email,
+            bool EmailConfirmed,
+            string PhoneNumber,
+            bool PhoneNumberConfirmed,
+            string DisplayName,
+            string Avatar);
+    }
+
+    public class Handler(
+        UserManager<AppUser> _userManager)
+        : IRequestHandler<Request, Either<Error, Response>>
+    {
+        private readonly UserManager<AppUser> _userManager = _userManager;
+
+        public async Task<Either<Error, Response>> Handle(Request request, CancellationToken cancellationToken)
+        {
+            var users = await _userManager.Users
+                .Map().ToListAsync(cancellationToken);
+            return new Response(users.AsReadOnly());
+        }
+    }
+
+    private static partial IQueryable<Response.User> Map(this IQueryable<AppUser> query);
+}
